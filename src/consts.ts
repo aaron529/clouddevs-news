459511ffb129650f26568eb8eb734e78de38ff0f
Aaron <correaaaron70@proton.me>
2025-02-28 11:46:04 +0530

    Astro deplo on Cloudflare

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CloudDevs.news';
+export const SITE_DESCRIPTION = "Welcome to CloudDevs.News, your go-to hub for everything tech! Whether you're a developer, innovator, or just a tech enthusiast, we bring you the latest updates, in-depth articles, and insights across the ever-evolving world of technology. From cloud computing to cutting-edge software developments, our blog covers it all. Stay ahead of the curve with news, trends, tutorials, and thought-provoking content that will keep you informed and inspired. Dive into the future of tech with CloudDevs.News—where innovation meets information.";
